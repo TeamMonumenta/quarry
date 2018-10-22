d39571713d70d49767e0b9e9085a2286e7ca0898
Tim Southwick <NickNackGus@gmail.com>
2018-10-21 20:22:54 -0400

    Added support for NBT is_subset comparison, NBT to NBT-json (with and without highlighting ala 1.13's /data get), NBT at_path (ala 1.13's /data get <container> <path>), and started work on an NBT-json to NBT method.

--- quarry/types/nbt.py
+++ quarry/types/nbt.py
@@ -10,12 +10,79 @@ _kinds = {}
 _ids = {}
 
 
+# Format codes
+formatCodes = {
+    "colors":'0123456789abcdef',
+    "styles":'klmnor',
+    "list":[
+        {"id":"0", "display":"Black",         "technical":"black",         "ansi":"\x1b[30m",   "foreground":0x000000, "background":0x000000},
+        {"id":"1", "display":"Dark Blue",     "technical":"dark_blue",     "ansi":"\x1b[34m",   "foreground":0x0000AA, "background":0x00002A},
+        {"id":"2", "display":"Dark Green",    "technical":"dark_green",    "ansi":"\x1b[32m",   "foreground":0x00AA00, "background":0x002A00},
+        {"id":"3", "display":"Dark Aqua",     "technical":"dark_aqua",     "ansi":"\x1b[36m",   "foreground":0x00AAAA, "background":0x002A2A},
+        {"id":"4", "display":"Dark Red",      "technical":"dark_red",      "ansi":"\x1b[31m",   "foreground":0xAA0000, "background":0x2A0000},
+        {"id":"5", "display":"Dark Purple",   "technical":"dark_purple",   "ansi":"\x1b[35m",   "foreground":0xAA00AA, "background":0x2A002A},
+        {"id":"6", "display":"Gold",          "technical":"gold",          "ansi":"\x1b[33m",   "foreground":0xFFAA00, "background":0x2A2A00},
+        {"id":"7", "display":"Gray",          "technical":"gray",          "ansi":"\x1b[37m",   "foreground":0xAAAAAA, "background":0x2A2A2A},
+        {"id":"8", "display":"Dark Gray",     "technical":"dark_gray",     "ansi":"\x1b[30;1m", "foreground":0x555555, "background":0x151515},
+        {"id":"9", "display":"Blue",          "technical":"blue",          "ansi":"\x1b[34;1m", "foreground":0x5555FF, "background":0x15153F},
+        {"id":"a", "display":"Green",         "technical":"green",         "ansi":"\x1b[32;1m", "foreground":0x55FF55, "background":0x153F15},
+        {"id":"b", "display":"Aqua",          "technical":"aqua",          "ansi":"\x1b[36;1m", "foreground":0x55FFFF, "background":0x153F3F},
+        {"id":"c", "display":"Red",           "technical":"red",           "ansi":"\x1b[31;1m", "foreground":0xFF5555, "background":0x3F1515},
+        {"id":"d", "display":"Light Purple",  "technical":"light_purple",  "ansi":"\x1b[35;1m", "foreground":0xFF55FF, "background":0x3F153F},
+        {"id":"e", "display":"Yellow",        "technical":"yellow",        "ansi":"\x1b[33;1m", "foreground":0xFFFF55, "background":0x3F3F15},
+        {"id":"f", "display":"White",         "technical":"white",         "ansi":"\x1b[37;1m", "foreground":0xFFFFFF, "background":0x3F3F3F},
+
+        {"id":"k", "display":"Obfuscated",    "technical":"obfuscated",    "ansi":"\x1b[7m",    },
+        {"id":"l", "display":"Bold",          "technical":"bold",          "ansi":"\x1b[1m",    },
+        {"id":"m", "display":"Strikethrough", "technical":"strikethrough", "ansi":"\x1b[9m",    },
+        {"id":"n", "display":"Underline",     "technical":"underlined",    "ansi":"\x1b[4m",    },
+        {"id":"o", "display":"Italic",        "technical":"italic",        "ansi":"\x1b[3m",    },
+        {"id":"r", "display":"Reset",         "technical":"reset",         "ansi":"\x1b[0m",    },
+    ],
+}
+
+def getColor(match,key=None):
+    """
+    Return one piece of color information by an ID or name
+    """
+    for format in formatCodes["list"]:
+        if (
+            str(match).lower() == format["id"] or
+            str(match).lower() == format["display"].lower() or
+            str(match).lower() == format["technical"]
+        ):
+            # Match found; return the specified part of the format,
+            # otherwise the whole format
+            return format.get(key,format)
+
+def unformatText(text):
+    """
+    Return the provided text without §-style format codes
+    """
+    while '§' in text:
+        i = text.find('§')
+        text = text[:i]+text[i+2:]
+    return text
+
+def ansifyText(text):
+    """
+    Return the provided text with §-style format codes converted to ansi format codes (compatible with most terminals)
+    """
+    while '§' in text:
+        i = text.find('§')
+        ansiCode = getColor(text[i+1],'ansi')
+        text = text[:i]+ansiCode+text[i+2:]
+    return text
+
 # Base types ------------------------------------------------------------------
 
 @functools.total_ordering
 class _Tag(object):
     def __init__(self, value):
         self.value = value
+
+    prefix = ('',getColor('gold','ansi'))
+    postfix = ('',getColor('reset','ansi'))
 
     @classmethod
     def from_bytes(cls, bytes):
@@ -40,6 +107,9 @@ class _Tag(object):
     def __lt__(self, other):
         return self.to_obj() < other.to_obj()
 
+    def to_json(self,sort=None,highlight=False):
+        return self.prefix[highlight] + str(self.value) + self.postfix[highlight]
+
 
 class _DataTag(_Tag):
     fmt = None
@@ -51,9 +121,16 @@ class _DataTag(_Tag):
     def to_bytes(self):
         return Buffer.pack(self.fmt, self.value)
 
+    def is_subset(self,other):
+        return self.value == other.value
+
+    def at_path(self,path):
+        return self
+
 
 class _ArrayTag(_Tag):
     fmt = None
+    separator = (',',getColor('white','ansi')+', ')
 
     @classmethod
     def from_buff(cls, buff):
@@ -65,34 +142,79 @@ class _ArrayTag(_Tag):
             Buffer.pack('i', len(self.value)) +
             Buffer.pack_array(self.fmt, self.value))
 
+    def is_subset(self,other):
+        if (
+            type(other) != type(self) or
+            len(other) != len(self)
+        ):
+            return False
+        for i in range(len(self.value)):
+            if self.value[i] != other.value[i]:
+                return False
+        return True
+
+    def to_json(self,sort=None,highlight=False):
+        content_json = []
+        for content in self.value:
+            content_json.append(content.to_json(sort,highlight))
+        return self.prefix[highlight] + self.separator[highlight].join(content_json) + self.postfix[highlight]
+
+    def at_path(self,path):
+        if path.startswith('['):
+            path = path[1:]
+        if not ']' in path:
+            raise IndexError( '] not in path "' + path + '"' )
+        if path.find(']') + 1 != len(path):
+            raise IndexError( str(type(self)) + ' cannot contain other tags, and is the end of path "' + path + '"' )
+        path = path[:-1]
+
+        index = -1
+        try:
+            index = int(path)
+        except:
+            raise IndexError( 'Path index is not an integer: ' + path )
+
+        if index < 0 or index >= len(self.value):
+            raise IndexError( 'Index ' + str(index) + ' not in range (' + str(len(self.value)) + ' entries)' )
+
+        return self.value[index]
+
 
 # NBT tags --------------------------------------------------------------------
 
 class TagByte(_DataTag):
     fmt = 'b'
+    postfix = ('b',getColor('red','ansi')+'b'+getColor('reset','ansi'))
 
 
 class TagShort(_DataTag):
     fmt = 'h'
+    postfix = ('s',getColor('red','ansi')+'s'+getColor('reset','ansi'))
 
 
 class TagInt(_DataTag):
     fmt = 'i'
+    postfix = ('',getColor('reset','ansi'))
 
 
 class TagLong(_DataTag):
     fmt = 'q'
+    postfix = ('l',getColor('red','ansi')+'l'+getColor('reset','ansi'))
 
 
 class TagFloat(_DataTag):
     fmt = 'f'
+    postfix = ('f',getColor('red','ansi')+'f'+getColor('reset','ansi'))
 
 
 class TagDouble(_DataTag):
     fmt = 'd'
+    postfix = ('d',getColor('red','ansi')+'d'+getColor('reset','ansi'))
 
 
 class TagString(_Tag):
+    prefix = ('"',getColor('white','ansi')+'"'+getColor('green','ansi'))
+    postfix = ('"',getColor('reset','ansi')+getColor('white','ansi')+'"'+getColor('reset','ansi'))
 
     @classmethod
     def from_buff(cls, buff):
@@ -103,24 +225,49 @@ class TagString(_Tag):
         data = self.value.encode('utf8')
         return Buffer.pack('H', len(data)) + data
 
+    def is_subset(self,other):
+        return self.value == other.value
+
+    def to_json(self,sort=None,highlight=False):
+        text = self.value.replace('\\','\\\\').replace('\n','\\n"').replace('"','\\"')
+        if highlight:
+            text = ansifyText(text)
+        return self.prefix[highlight] + text + self.postfix[highlight]
+
 
 class TagByteArray(_ArrayTag):
     fmt = 'b'
+    prefix = ('[B;',getColor('white','ansi')+'['+getColor('red','ansi')+'B'+getColor('white','ansi')+'; '+getColor('gold','ansi'))
+    postfix = ('b]',getColor('red','ansi')+'b'+getColor('white','ansi')+']'+getColor('reset','ansi'))
+    separator = ('b,',getColor('red','ansi')+'b'+getColor('white','ansi')+', '+getColor('gold','ansi'))
 
 
 class TagIntArray(_ArrayTag):
     fmt = 'i'
+    prefix = ('[I;',getColor('white','ansi')+'['+getColor('red','ansi')+'I'+getColor('white','ansi')+'; '+getColor('gold','ansi'))
+    postfix = (']',getColor('white','ansi')+']'+getColor('reset','ansi'))
+    separator = (',',getColor('white','ansi')+', '+getColor('gold','ansi'))
 
 
 class TagLongArray(_ArrayTag):
     fmt = 'q'
+    prefix = ('[L;',getColor('white','ansi')+'['+getColor('red','ansi')+'L'+getColor('white','ansi')+'; '+getColor('gold','ansi'))
+    postfix = ('l]',getColor('red','ansi')+'l'+getColor('white','ansi')+']'+getColor('reset','ansi'))
+    separator = ('l,',getColor('red','ansi')+'l'+getColor('white','ansi')+', '+getColor('gold','ansi'))
 
 
 class TagUnsignedLongArray(_ArrayTag):
     fmt = 'Q'
+    prefix = ('[L;',getColor('white','ansi')+'['+getColor('red','ansi')+'L'+getColor('white','ansi')+'; '+getColor('gold','ansi'))
+    postfix = ('l]',getColor('red','ansi')+'l'+getColor('white','ansi')+']'+getColor('reset','ansi'))
+    separator = ('l,',getColor('red','ansi')+'l'+getColor('white','ansi')+', '+getColor('gold','ansi'))
 
 
 class TagList(_Tag):
+    prefix = ('[',getColor('white','ansi')+'['+getColor('gold','ansi'))
+    postfix = (']',getColor('white','ansi')+']'+getColor('reset','ansi'))
+    separator = (',',getColor('white','ansi')+', '+getColor('gold','ansi'))
+
     @classmethod
     def from_buff(cls, buff):
         inner_kind_id, array_length = buff.unpack('bi')
@@ -139,10 +286,52 @@ class TagList(_Tag):
     def to_obj(self):
         return [tag.to_obj() for tag in self.value]
 
+    def is_subset(self,other):
+        if type(other) != TagList:
+            return False
+        for self_value in self.value:
+            if not any(self_value.is_subset(other_value) for other_value in other.value):
+                return False
+        return True
+
+    def to_json(self,sort=None,highlight=False):
+        content_json = []
+        for content in self.value:
+            content_json.append(content.to_json(sort,highlight))
+        return self.prefix[highlight] + self.separator[highlight].join(content_json) + self.postfix[highlight]
+
+    def at_path(self,path):
+        if path.startswith('['):
+            path = path[1:]
+        if not ']' in path:
+            raise IndexError( '] not in path "' + path + '"' )
+        if ']' not in path:
+            raise IndexError( 'Could not find "]" in "' + path + '"' )
+
+        split_index = path.find(']')
+        array_index = path[:split_index]
+        path = path[split_index+1:]
+
+        try:
+            array_index = int(array_index)
+        except:
+            raise IndexError( 'Array index is not an integer: ' + array_index )
+
+        if array_index < 0 or array_index >= len(self.value):
+            raise IndexError( 'Index ' + str(array_index) + ' not in range (' + str(len(self.value)) + ' entries)' )
+
+        if path == '':
+            return self.value[array_index]
+        else:
+            return self.value[array_index].at_path(path)
 
 class TagCompound(_Tag):
     root = False
     preserve_order = False
+    prefix = ('{',getColor('white','ansi')+'{'+getColor('gold','ansi'))
+    postfix = ('}',getColor('white','ansi')+'}'+getColor('reset','ansi'))
+    separator = (',',getColor('white','ansi')+', '+getColor('gold','ansi'))
+    key_value_separator = (':',getColor('white','ansi')+': ')
 
     @classmethod
     def from_buff(cls, buff):
@@ -198,6 +387,173 @@ class TagCompound(_Tag):
             else:
                 self.value[name] = new_tag
 
+    def is_subset(self,other):
+        if type(other) != TagCompound:
+            return False
+        for aKey in self.value.keys():
+            if (
+                aKey not in other.value.keys() or
+                not self.value[aKey].is_subset(other.value[aKey])
+            ):
+                return False
+        return True
+
+    def to_json(self,sort=None,highlight=False):
+        if type(sort) in (list,tuple):
+            keys = []
+            for key in sort:
+                if key in self.value.keys():
+                    keys.append(key)
+            for key in self.value.keys():
+                if key not in sort:
+                    keys.append(key)
+        else:
+            keys = self.value.keys()
+
+        content_json = []
+        for key in keys:
+            content = self.value[key]
+            content_json.append( key + self.key_value_separator[highlight] + content.to_json(sort,highlight) )
+        return self.prefix[highlight] + self.separator[highlight].join(content_json) + self.postfix[highlight]
+
+    def at_path(self,path):
+        if path.startswith('.') or path.startswith('['):
+            raise KeyError( path + 'not in ' + str(self) )
+        split_index = len(path)
+        bracket_index = path.find('[')
+        dot_index = path.find('.')
+
+        if dot_index >= 0:
+            split_index = min(split_index,dot_index)
+        if bracket_index >= 0:
+            split_index = min(split_index,bracket_index)
+
+        key = path[:split_index]
+        path = path[split_index+1:]
+        if key not in self.value:
+            raise KeyError( 'key "' + key + '" not in ' + str(self) )
+
+        if path == '':
+            return self.value[key]
+        else:
+            return self.value[key].at_path(path)
+
+'''
+    @classmethod
+    def from_json(cls,json):
+        """
+        Convert a json-style NBT string into NBT
+        """
+        debug = ''
+        startQuote = None
+        charsToIgnore = 0
+
+        def next_type(json):
+            best = None
+            best_pre = ''
+            for kind in _kinds.values():
+                if (
+                    'prefix' not in dir(kind) or
+                    len(kind.prefix[0]) == 0 or
+                    not json.startswith(kind.prefix[0]) or
+                    len(kind.prefix[0]) < len(best_pre)
+                ):
+                    continue
+                best = kind
+                best_pre = kind.prefix[0]
+            return(best)
+
+        class parse_state(object):
+            """
+            The state of the current value being parsed;
+            used to simplify adding values to tags, or
+            to finish adding values to a tag.
+            """
+            def __init__(self):
+                self.next_name = ''
+                self.next_value = None
+                self.result = TagCompound({})
+                self.stack = [self.result]
+
+            def store_tag(self):
+                # Add current tag to the parent tag
+                parent = self.stack[-1]
+                if type(parent) is TagCompound:
+                    # parent is TagCompound
+                    parent[self.next_name] = self.next_value
+
+                elif type(parent) is TagList:
+                    # parent is TagList
+                    parent.append(self.next_value)
+
+                elif isinstance(tag, _ArrayTag):
+                    # parent is a numeric array
+                    parent.append(self.next_value.value)
+
+                else:
+                    raise TypeError("Unexpected parent data type while parsing NBT json")
+
+                # Add new tag to stack if it's a container
+                child = self.next_value
+                if (
+                    type(child) is TagCompound or
+                    type(child) is TagList or
+                    isinstance(tag, _ArrayTag)
+                ):
+                    self.stack.append(child)
+
+                # Reset current tag state
+                self.next_name = ''
+                self.next_value = None
+
+        state = parse_state()
+
+        # Begin parse
+        for i,c in enumerate(json):
+            # i is the index of character c in json
+            if charsToIgnore > 0:
+                debug += c
+                charsToIgnore -= 1
+                continue
+            elif c == '\\':
+                # This charcter is a \, ignore next character
+                debug += '\\'
+                charsToIgnore = 1
+                continue
+            elif c == '"':
+                # Quote found, is it start or end?
+                debug += '"'
+                if startQuote is not None:
+                    # It is an end quote, accept the value
+                    # Include the quote marks to identify type
+
+                    # Note that this might in fact be the tag NAME,
+                    # not the tag VALUE. This will be updated when a
+                    # colon signifies the value starts next, or when
+                    # it is clear the end of the tag has arrived.
+                    cls.currentValue = json[startQuote: i + 1]
+                    startQuote = None
+                    continue
+                else:
+                    # It is a start quote, record the location
+                    debug += '"'
+                    startQuote = i
+                    continue
+            elif startQuote is not None:
+                # We're inside quotes; other cases should be ignored.
+                debug += '~'
+                continue
+            elif c == '{':
+                # New compound tag
+                if i == 0:
+                    # This is the starting tag, and accounted for.
+                    debug += '{'
+                    continue
+                else:
+                    # This tag is not accounted for.
+                    debug += '{'
+                    #
+'''
 
 class TagRoot(TagCompound):
     root = True
